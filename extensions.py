96d3aa85f671fcc23a12d250fb712bf2f93f7d2e
test <test>
2023-11-05 14:26:36 +0300

    test

---
+++ extensions.py
@@ -0,0 +1,19 @@
+def main():
+    x=input("File name: ")
+    if x.endswith(".gif"):
+        print("image/gif")
+    elif x.endswith(".jpeg"):
+         print("image/jpeg")
+    elif x.endswith(".png"):
+         print("image/png")
+    elif x.endswith(".jpg"):
+          print("image/jpeg")
+    elif x.endswith(".pdf"):
+          print("application/pdf")
+    elif x.endswith(".zip"):
+          print("application/zip")
+    elif x.endswith(".txt"):
+          print("text/plain")
+    else:
+        print("application/octet-stream")
+main()
